51183d6564de658b0efde0fb8db25c7b53a753a3
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2024-04-05 18:07:39 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+import streamlit as st
+import google.generativeai as genai
+import google.ai.generativelanguage as glm
+
+# APIキーを環境変数から読み込む
+api_key = os.environ.get("GENAI_API_KEY")
+
+# APIキー設定
+genai.configure(api_key=api_key)
+
+# タイトルを設定する
+st.set_page_config(
+    page_title="Chat with Gemini 1.5Pro",
+    page_icon="🐤"
+)
+
+st.title("🐤 Chat with Gemini 1.5Pro")
+
+# セッション状態の初期化
+if "chat_session" not in st.session_state:
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    st.session_state["chat_session"] = model.start_chat(history=[
+        glm.Content(role="user", parts=[glm.Part(text="あなたは優秀なAIアシスタントです。どのような話題も適切に詳細に答えます。時々偉人や哲学者の名言を引用します。")]),
+        glm.Content(role="model", parts=[glm.Part(text="わかりました。")])
+    ])
+    st.session_state["chat_history"] = []
+
+# チャット履歴を全て表示
+for message in st.session_state["chat_history"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# ユーザー入力送信後処理
+if prompt := st.chat_input("ここに入力してください"):
+    # ユーザの入力を表示する
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # ユーザの入力をチャット履歴に追加する
+    st.session_state["chat_history"].append({"role": "user", "content": prompt})
+
+    # Genimi Proにメッセージ送信
+    response = st.session_state["chat_session"].send_message(prompt)
+
+    # Genimi Proのレスポンス表示
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+
+    # Genimi Proのレスポンスをチャット履歴に追加する
+    st.session_state["chat_history"].append({"role": "assistant", "content": response.text})
+
+if __name__ == "__main__":
+    from streamlit.web.cli import main
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        # Streamlitアプリケーションを実行する
+        try:
+            main()
+        except SystemExit as e:
+            if e.code != 0:
+                return 'Error', 500
+        except Exception as e:
+            # その他の例外が発生した場合のエラーハンドリング
+            return str(e), 500
+        # 正常終了時のレスポンスを返す
+        return 'OK', 200
+
+    app.run(port=8080)
